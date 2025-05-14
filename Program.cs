3abcc0b34e9e83d0c6b6231e574c2e95905ea8e0
Sofi <esalexandr67@gmail.com>
2025-05-15 00:54:57 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 class Point
 {
     public double X, Y;
-    public Point(double x, double y) { X = x; Y = Y; }
+    public Point(double x, double y) { X = x; Y = Y; } // Исправить "Y" на "y" во втором случае 
 
     public static double Distance(Point p1, Point p2) =>
         Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2));
@@ -39,7 +39,7 @@ class IsoscelesTrapezoid
     {
         double base1 = Point.Distance(A, B);
         double base2 = Point.Distance(C, D);
-        double height = Math.Abs(A.Y - A.Y); // допущение: основания горизонтальны
+        double height = Math.Abs(A.Y - A.Y); // "A" во втором случае исправить на "D"
         return 0.5 * (base1 + base2) * height;
     }
 }
@@ -61,6 +61,7 @@ class Program
 
         Console.WriteLine($"Равнобочных трапеций: {valid.Count}");
         Console.WriteLine($"Средняя площадь: {avgArea}");
-        Console.WriteLine($"С площадью выше средней: {avgArea}");
+        Console.WriteLine($"С площадью выше средней: {avgArea}");//В "средней площади" добавить F2 для более приятного отображения, в "c площадью выше средней" исправить на countAboveAvg
+
     }
 }
